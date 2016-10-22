f21e461e82c4182db95aa0fd22c1b25a396f17f2
merryHunter <chernuhaiv@gmail.com>
2016-10-22 22:16:26 +0300

    dashboard init

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == '__main__':
     app.run()
