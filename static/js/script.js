1d1a6684e5e066b5f918b6987721d35ac8c5db98
user <user@debian>
2016-10-23 13:40:18 +0300

    landing js

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,17 @@
 var goods = ["better air quality", "reduced noise levels", "balanced lighting", "happier employees", "reduced stress", "healthier lifestyles"];
 
-text = document.getElementById('goodtext');
+var text = document.getElementById('goodtext');
 
-window.onload = function start() {
-    iterateMessages();
-}
 function iterateMessages() {
-    var num = 0,
+    var num = 0;
     window.setInterval(function () {
-        text = goods[num];
+        text = "Performance through "+goods[num];
         if (num >= goods.length) {
-            num = 0
+            num = 0;
         }
     }, 3000);
 }
 
-for (i = 0; i < goods.length; i++) {
-    text = goods[i];
+window.onload = function start() {
+    iterateMessages();
 }
